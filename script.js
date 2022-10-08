2e0c334a49eff00fcbe2d8be8bac95c0810c7a48
Rajas Shah <rajas.s.shah@gmail.com>
2022-10-08 02:14:12 -0600

    Clean up

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const pass = document.querySelector('#password')
 const confirmPass = document.querySelector('#confirm-password')
 
-// confirmPass.addEventListener("input", (e) => {
-//     if (confirmPass.textContent !== pass.textContent) {
-//         confirmPass.pes
-//     }
-// });
+confirmPass.addEventListener("input", (e) => {
+    if (confirmPass.textContent !== pass.textContent) {
+        console.log("*Passwords do not match!")
+    }
+});
