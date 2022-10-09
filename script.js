70004857449a1ea558a6788ddebb0588521e679e
Rajas Shah <rajas.s.shah@gmail.com>
2022-10-08 19:09:02 -0600

    Improve password matching feature

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const pass = document.querySelector('#password')
 const confirmPass = document.querySelector('#password-confirm')
 const confirmPassText = document.querySelector('#password-confirm+span')
+var confirmPassVisited = false;
 
 function checkPassMatch() {
     confirmPass.setAttribute("pattern", pass.value);
@@ -11,5 +12,17 @@ function checkPassMatch() {
     }
 }
 
-confirmPass.addEventListener("input", checkPassMatch);
-pass.addEventListener("input", checkPassMatch);
+confirmPass.addEventListener("input", () => {
+    checkPassMatch();
+    confirmPassVisited = true;
+});
+
+confirmPass.addEventListener("focus", () => {
+    confirmPassVisited = true;
+});
+
+pass.addEventListener("input", () => {
+    if (confirmPassVisited) {
+        checkPassMatch();
+    }
+});
